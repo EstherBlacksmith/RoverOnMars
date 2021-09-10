a1f1efb5c72f1f64dc9dbaad060236da85c866e7
EstherBlackSmith <githubesther@gmail.com>
2021-09-10 19:40:44 +0200

    Initial Mars view

---
+++ resources/views/mars.blade.php
@@ -0,0 +1,29 @@
+<body>
+<div class="container">
+<div class="row">
+  <div class="col-2" style="background-color: red;">
+    <form>
+      <div class="mb-3">
+        <label for="movement" class="form-label">Movement</label>
+        <input type="text" class="form-control" id="movement" aria-describedby="Movement">
+        <div id="movementHelp" class="form-text">Introduce the movement for the ROver, for example AALARALLAA.</div>
+      </div>
+  
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+
+  <div class="col-2">
+    <img src="{{asset('images/mars.jpg')}}" class="img-fluid" alt="Riotinto">
+  </div>
+  
+</div>
+</div>
+
+</body>
+<footer>
+<p>
+  Photo by <a href="https://unsplash.com/@victor_g?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Victor</a> on <a href="https://unsplash.com/s/photos/mars?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+</p>
+
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/mars',function() {
+    return view('mars');
+});
