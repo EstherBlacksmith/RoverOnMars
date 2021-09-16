b5ce58f13d444ba90afc70f8ae8030eef6921a00
Esther Blacksmith <githubesther@gmail.com>
2021-09-16 15:19:10 +0200

    lo de clase

--- resources/views/mars.blade.php
+++ resources/views/mars.blade.php
@@ -25,50 +25,57 @@
   }
 </style>
 <div class="container">
+  <div class="h1">
+    MARS
+
+  </div>
   <div class="row">
     <div class="col-sm-4">
 
       <form  method="post" action="{{ route('movement') }}">
-        @csrf
-        <div>
-          <label for="initialPositionX" class="form-label">Initial position X</label>
-          <input type="text" class="form-control" id="initialPositionX" name="initialPositionX" aria-describedby="Initial position X">
-          <div id="initialPositionXHelp" class="form-text">Introduce the initial position in the axis X, for example 0 .</div>
-        </div>
+        @csrf      
 
         <div>
-          <label for="initialPositionY" class="form-label">Initial position Y</label>
-          <input type="text" class="form-control" id="initialPositionY" name="initialPositionY" aria-describedby="Initial position Y">
-          <div id="initialPositionYHelp" class="form-text">Introduce the initial position in the axis Y, for example 1 .</div>
-        </div>
+          <p>Select the size for the square for the Rover movement</p>
 
-        <div>
-          <label for="movement" class="form-label">Movement:  <span style="color:#ffa600">A</span>dvance, Turn  <span style="color:#ffa600">L</span>eft, Turn  <span style="color:#ffa600">R</span>ight. </label>
-          <input type="text" class="form-control" id="movement" name="movement" aria-describedby="Movement">
-          <div id="movementHelp" class="form-text">Introduce the movement for the Rover, for example <span style="color:#ffa600">AALARALLAA</span>.</div>
+          <label for="XRange" class="form-label"></label>
+          <input type="range" class="form-range mb-3" min="2" max="10" step="1" value="2" id="XRange">
+          <p>X Value: <span id="Xvalue"></span></p>
+
+          <label for="YRange" class="form-label"></label>
+          <input type="range" class="form-range mb-3" min="2" max="10" step="1" value="2" id="YRange">
+          <p>Y Value: <span id="Yvalue"></span></p>
+     
+          <button type="button" class="btn btn-success btn-lg mb-3" onclick="btnSquare()">Mars</button>
         </div>
-    
-      
-        <div>
-          <p>Create the square for the Rover movement</p>
-          <select class="form-select  form-select-sm" id="SelectColumns">
-            <option selected>Select colums</option>
+        <div class="col-sm-6">
+          <select class="form-select form-select mb-2" id="initialPositionX"  name="initialPositionX">
+            <option selected>Initial position X</option>
+            <div id="initialPositionXHelp" class="form-text">Introduce the initial position in the axis X, for example 0 .</div>
             @for ($colum = 2; $colum < 11; $colum++)
             <option value="{{$colum}}">{{{$colum}}}</option>      
             @endfor
           </select>
           
-          <select class="form-select form-select-sm" id="SelectRows">
-            <option selected>Select rows</option>
+          <select class="form-select form-select mb-3" id="initialPositionY" name="initialPositionY">
+            <option selected>Initial position Y</option>
+            <div id="initialPositionXHelp" class="form-text">Introduce the initial position in the axis X, for example 0 .</div>
             @for ($row = 2; $row < 11; $row++)
             <option value="{{$row}}">{{{$row}}}</option>      
             @endfor
           </select>
-          <button type="button" class="btn btn-primary" onclick="btnSquare()">Mars</button>
+          <button type="button" class="btn btn-success btn-lg mb-3" onclick="btnRover()">Rover</button>
+
+        </div>  
+
+        <div>
+          <label for="movement" class="form-label">Movement:  <span style="color:#ffa600">A</span>dvance, Turn  <span style="color:#ffa600">L</span>eft, Turn  <span style="color:#ffa600">R</span>ight. </label>
+          <input type="text" class="form-control mb-3" id="movement" name="movement" aria-describedby="Movement">
+          <div id="movementHelp" class="form-text  mb-3">Introduce the movement for the Rover, for example <span style="color:#ffa600">AALARALLAA</span>.</div>
         </div>
 
-        <button type="submit" class="btn btn-primary">Submit</button>
-        <input class="btn btn-primary" type="reset" value="Reset" onclick="btnReset()">
+        <button type="submit" class="btn btn-primary mb-3">Submit</button>
+        <input class="btn btn-primary mb-3" type="reset" value="Reset" onclick="btnReset()">
 
       </form>    
     </div>
@@ -91,16 +98,45 @@
 
 </body>
 <script>
-  function btnSquare() {
+
+  var sliderX = document.getElementById("XRange");
+  var outputX = document.getElementById("Xvalue");
+  var sliderY = document.getElementById("YRange");
+  var outputY = document.getElementById("Yvalue");
+
+
+    outputX.innerHTML = sliderX.value; 
+
+    sliderX.oninput = function() {
+      outputX.innerHTML = this.value;
+    }    
+ 
+    outputY.innerHTML = sliderY.value;
+   
+    sliderY.oninput = function() {
+      outputY.innerHTML = this.value;
+    }
+  
+  function btnRover(){
     
-    btnReset();
+    btnSquare();   
 
-    var selectRows = document.getElementById('SelectRows');
-    var SelectColumns = document.getElementById('SelectColumns');
+    var initialX = document.getElementById('initialPositionX').value -1;
+    var initialY = document.getElementById('initialPositionY').value -1;
+   // document.getElementById(initialX + "_" + initialY).style.backgroundColor = 'blue';
+    document.getElementById(initialX + "_" + initialY).style.backgroundImage = "url('images/mars.jpg')";
 
-    var valueRows = selectRows.options[selectRows.selectedIndex].value;
-    var valueColumns = SelectColumns.options[SelectColumns.selectedIndex].value;
 
+
+  }
+
+  function btnSquare() { 
+
+    var valueRows = document.getElementById('XRange').value;
+    var valueColumns = document.getElementById('YRange').value;
+    
+    greyCells();
+    
     for (r = 0; r < valueRows; r++) //rows    
     {   
       for (c = 0; c < valueColumns; c++) //columns
@@ -110,14 +146,21 @@
     }
   }
 
-  function btnReset() {  
+  function btnReset() { 
+    
+    greyCells();  
 
+    outputX.innerHTML = 2;
+    outputY.innerHTML = 2;
+  }
+
+  function greyCells(){
     for (t = 0; t < 100; t++) //rows    
     {  
       document.getElementsByClassName("grid-item")[t].style.backgroundColor = 'darkGrey';
     }
-    
   }
+
   </script>
 
 <footer>
